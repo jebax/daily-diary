0da98b805c166c068f59a896c87fb1f7c5d65639
jebax <joel.eliot.baxter@gmail.com>
2018-10-23 10:04:46 +0100

    entry title can be filled in but not submitted yet

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,6 +1,14 @@
 feature "home page" do
-  scenario "user can see that they are using a diary app" do
+  before do
     visit '/'
+  end
+
+  scenario "user can see that they are using a diary app" do
     expect(page).to have_content "My Diary"
+  end
+
+  scenario "user can fill in a title's entry" do
+    expect(page).to have_content "Your entry title:"
+    expect(page).to have_css("input[type='text']")
   end
 end
