fa1552e2c1b3d2963d139ebc20703ff72da0c33e
jebax <joel.eliot.baxter@gmail.com>
2018-10-23 09:52:02 +0100

    created first page

--- app.rb
+++ app.rb
@@ -2,5 +2,9 @@ require 'sinatra/base'
 
 class DailyDiary < Sinatra::Base
 
+  get '/' do
+    erb :home
+  end
+  
   run! if app_file == $0
 end
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,4 +1,4 @@
-feature "index page" do
+feature "home page" do
   scenario "user can see that they are using a diary app" do
     visit '/'
     expect(page).to have_content "My Diary"
