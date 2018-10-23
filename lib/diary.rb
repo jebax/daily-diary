9ba7d69cbec75a2ae1c362d4582e3771c29e064e
jebax <joel.eliot.baxter@gmail.com>
2018-10-23 21:21:57 +0100

    refactored diary class

--- lib/diary.rb
+++ lib/diary.rb
@@ -1,13 +1,27 @@
 require 'pg'
 
 class Diary
+
+  @database = nil
+
   def self.all_entries
+    choose_database
+    select_all.map { |entry| entry['title'] }
+  end
+
+  private
+
+  attr_reader :database
+
+  def self.choose_database
     if ENV['ENVIRONMENT'] = 'test'
       database = PG.connect(dbname: 'daily_diary_test')
     else
       database = PG.connect(dbname: 'daily_diary')
     end
-    all = database.exec('select * from diary')
-    all.map { |entry| entry['title'] }
+  end
+
+  def self.select_all
+    database.exec('select * from diary')
   end
 end
